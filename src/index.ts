48b7897aab325c00034aa8aa54977829624fa737
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-07 14:40:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let btnEnv = document.getElementById("enviar");
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+
+rotulo1.innerHTML = "Precio del producto $450,50";
+rotulo2.innerHTML = "Descuento10%";
+
+btnEnv.addEventListener("click", () => {
+  let dato1 = 450.0;
+  let descuento: number = 10;
+  let descuento: number = dato1 - dato1 * 0.1;
+  console.log(
+    "El precio final de su compra con descuento es de",
+    descuento,
+    "pesos"
+  );
+});
