65fb347996ba46b09a557b137b68feb7b05d15d7
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-21 20:35:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+/*Ejercicio: Cálculo de Descuento
+55
+• Implemente un algoritmo que calcule y
+muestre por pantalla el precio final de un
+producto después de aplicarle un descuento
+• El precio inicial del producto es $450,50
+• El descuento a aplicar es del 10%.
+Recuerde que puede obtener el 10% de un valor
+multiplicando por 0,1
+• El precio y el descuento deben ser guardados en
+variables (no ingresados por teclado)*/
+
 let btnEnv = document.getElementById("enviar");
 let rotulo1 = document.getElementById("rotulo1");
 let rotulo2 = document.getElementById("rotulo2");
